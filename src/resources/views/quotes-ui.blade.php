232b33ac30197fd601c1921f6d40022e2f7a5dd1
Steven Sucre <steven.g.s.p@gmail.com>
2025-03-20 15:35:44 -0400

    Changes

--- src/resources/views/quotes-ui.blade.php
+++ src/resources/views/quotes-ui.blade.php
@@ -12,25 +12,25 @@
         <div v-if="quotes.length">
             <h2>All Quotes</h2>
             <div v-for="quote in quotes" :key="quote.id">
-                <p>{{ quote.quote }} - {{ quote.author }}</p>
+                <p>@{{ quote.quote }} - @{{ quote.author }}</p>
             </div>
 
             <div v-if="totalPages > 1">
                 <button @click="fetchNextPageQuotes" :disabled="currentPage >= totalPages">
                     Load More Quotes
                 </button>
-                <p>Page {{ currentPage }} of {{ totalPages }}</p>
+                <p>Page @{{ currentPage }} of @{{ totalPages }}</p>
             </div>
         </div>
 
         <h2>Random Quote</h2>
         <button @click="fetchRandomQuote">Get Random Quote</button>
-        <p v-if="quote">{{ quote.quote }} - {{ quote.author }}</p>
+        <p v-if="quote">@{{ quote.quote }} - @{{ quote.author }}</p>
 
         <h2>Get Quote by ID</h2>
         <input v-model="quoteId" placeholder="Enter Quote ID" />
         <button @click="fetchQuoteById">Get Quote</button>
-        <p v-if="quoteById">{{ quoteById.quote }} - {{ quoteById.author }}</p>
+        <p v-if="quoteById">@{{ quoteById.quote }} - @{{ quoteById.author }}</p>
     </div>
 
     <script type="module" src="{{ asset('vendor/quotes-api/assets/index.js') }}"></script>
