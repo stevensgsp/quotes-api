5a1c54ee6d8c73d82e5ee28bd841d8fdb2b57fe4
Steven Sucre <steven.g.s.p@gmail.com>
2025-03-20 02:10:10 -0400

    Fixes

--- src/config/quotes.php
+++ src/config/quotes.php
@@ -1,6 +1,6 @@
-<?
+<?php
 
 return [
-    'base_url' => env('QUOTES_API_BASE_URL', 'https://dummyjson.com'),
-    'rate_limit' => env('QUOTES_API_RATE_LIMIT', 60),
+    'base_url' => 'https://dummyjson.com',
+    'rate_limit' => 60,
 ];
