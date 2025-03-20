a350c0e57797b1a651ebb0254c69baa9b67b2c2d
Steven Sucre <steven.g.s.p@gmail.com>
2025-03-20 13:22:02 -0400

    Changes

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Stevensgsp\QuotesApi\Http\Controllers\QuoteController;
 
-Route::prefix('quotes')->group(function () {
+Route::prefix('api/quotes')->group(function () {
     Route::get('/', [QuoteController::class, 'index']);
     Route::get('/random', [QuoteController::class, 'random']);
     Route::get('/{id}', [QuoteController::class, 'show']);
