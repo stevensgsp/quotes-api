042a5d9e7832e5b5240a9d72050070d2d37d7aba
Steven Sucre <steven.g.s.p@gmail.com>
2025-03-20 15:37:49 -0400

    Changes

--- src/routes/api.php
+++ src/routes/api.php
@@ -6,7 +6,7 @@ use Stevensgsp\QuotesApi\Http\Controllers\QuoteController;
 Route::prefix('api/quotes')->group(function () {
     Route::get('/', [QuoteController::class, 'index']);
     Route::get('/random', [QuoteController::class, 'random']);
-    Route::get('/{id}', [QuoteController::class, 'show']);
     Route::get('/next', [QuoteController::class, 'nextPage']);
     Route::get('/total-pages', [QuoteController::class, 'totalPages']);
+    Route::get('/{id}', [QuoteController::class, 'show']);
 });
