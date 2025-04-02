1aaead45c2410dd749cb4aa5e6ace161a5dbcb89
Steven Sucre <steven.g.s.p@gmail.com>
2025-04-02 19:05:05 -0400

    Added router link and css.

--- src/resources/js/App.vue
+++ src/resources/js/App.vue
@@ -1,9 +1,16 @@
 <template>
-    <div>
-        <p>hola</p>
-        <router-link to="/">List</router-link>
-        <router-link to="/get-by-id">Get By Id</router-link>
-        <router-link to="/random">Random</router-link>
-        <router-view></router-view>
+    <div class="flex min-h-screen bg-gray-100">
+        <aside class="w-64 bg-blue-600 text-white p-4 space-y-4">
+            <h1 class="text-2xl font-bold">Quotes App</h1>
+            <nav class="space-y-2">
+                <router-link to="/" class="block px-4 py-2 rounded-lg hover:bg-blue-500">List</router-link>
+                <router-link to="/get-by-id" class="block px-4 py-2 rounded-lg hover:bg-blue-500">Get By Id</router-link>
+                <router-link to="/random" class="block px-4 py-2 rounded-lg hover:bg-blue-500">Random</router-link>
+            </nav>
+        </aside>
+
+        <main class="flex-1 p-6">
+            <router-view></router-view>
+        </main>
     </div>
 </template>
