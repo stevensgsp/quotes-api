96f4ed443095aff1d816bd813f5f3045aae3aed7
Steven Sucre <steven.g.s.p@gmail.com>
2025-04-03 12:59:42 -0400

    Adding useRoute

--- src/resources/js/App.vue
+++ src/resources/js/App.vue
@@ -3,9 +3,23 @@
         <aside class="w-64 bg-blue-600 text-white p-4 space-y-4">
             <h1 class="text-2xl font-bold">Quotes App</h1>
             <nav class="space-y-2">
-                <router-link to="/" class="block px-4 py-2 rounded-lg hover:bg-blue-500">List</router-link>
-                <router-link to="/get-by-id" class="block px-4 py-2 rounded-lg hover:bg-blue-500">Get By Id</router-link>
-                <router-link to="/random" class="block px-4 py-2 rounded-lg hover:bg-blue-500">Random</router-link>
+                <router-link
+                    to="/"
+                    class="block px-4 py-2 rounded-lg hover:bg-blue-500"
+                    :class="{ 'bg-blue-500': route.path === '/' }"
+                >List</router-link>
+
+                <router-link
+                    to="/get-by-id"
+                    class="block px-4 py-2 rounded-lg hover:bg-blue-500"
+                    :class="{ 'bg-blue-500': route.path === '/get-by-id' }"
+                >Get By Id</router-link>
+
+                <router-link
+                    to="/random"
+                    class="block px-4 py-2 rounded-lg hover:bg-blue-500"
+                    :class="{ 'bg-blue-500': route.path === '/random' }"
+                >Random</router-link>
             </nav>
         </aside>
 
@@ -14,3 +28,10 @@
         </main>
     </div>
 </template>
+
+
+<script setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+</script>
