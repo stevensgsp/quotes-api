45de9771e3884123f317570f73d7455ab4f385d5
Steven Sucre <steven.g.s.p@gmail.com>
2025-04-03 17:46:21 -0400

    Added new tests.

--- tests/Feature/QuotesApiTest.php
+++ tests/Feature/QuotesApiTest.php
@@ -18,18 +18,35 @@ class QuotesApiTest extends TestCase
     public function test_fetch_all_quotes()
     {
         $response = $this->get('/api/quotes');
+
         $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'quotes' => [
+                '*' => ['id', 'quote', 'author'],
+            ],
+        ]);
     }
 
     public function test_fetch_random_quote()
     {
         $response = $this->get('/api/quotes/random');
+
         $response->assertStatus(200);
+        $response->assertJsonStructure(['id', 'quote', 'author']);
     }
 
     public function test_fetch_specific_quote()
     {
         $response = $this->get('/api/quotes/1');
+
         $response->assertStatus(200);
+        $response->assertJsonStructure(['id', 'quote', 'author']);
+    }
+
+    public function test_fetch_non_existent_quote_returns_404()
+    {
+        $response = $this->get('/api/quotes/999999');
+
+        $response->assertStatus(404);
     }
 }
